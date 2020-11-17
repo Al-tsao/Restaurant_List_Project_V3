7604bbde38d8b1248eea77ececb187a4e80447a4
AlTsao <al.tsao.yw@gmail.com>
2020-11-17 13:11:28 +0800

    refactor: modify express routes

--- app.js
+++ app.js
@@ -6,7 +6,8 @@ const mongoose = require('mongoose')
 const Restaurant = require('./models/rList')
 // 載入 method-override
 const methodOverride = require('method-override')
-
+// 引用路由器
+const routes = require('./routes')
 
 // set express to app
 const app = express()
@@ -21,6 +22,9 @@ app.engine('handlebars', exphbs({ defaultLayout: 'main' }))
 app.use(express.static('public'), bodyParser.urlencoded({ extended: true }))
 // 設定每一筆請求都會透過 methodOverride 進行前置處理
 app.use(methodOverride('_method'))
+// 將 request 導入路由器
+app.use(routes)
+
 
 // connect to database
 mongoose.connect('mongodb://localhost/r_list', { useNewUrlParser: true, useUnifiedTopology: true })
@@ -39,100 +43,100 @@ db.once('open', () => {
 // ---------------- route setting ------------------
 
 // index page requesting
-app.get('/', (req, res) => {
-  Restaurant.find()
-    .lean()
-    .then(restaurants => {
-      res.render('index', { restaurants })
-    })
-    .catch(error => console.log(error))
-})
+// app.get('/', (req, res) => {
+//   Restaurant.find()
+//     .lean()
+//     .then(restaurants => {
+//       res.render('index', { restaurants })
+//     })
+//     .catch(error => console.log(error))
+// })
 
-// show page requesting
-app.get('/restaurants/:id', (req, res) => {
-  const id = req.params.id
-  Restaurant.findById(id)
-    .lean()
-    .then(restaurants => {
-      res.render('show', { restaurants })
-    })
-    .catch(error => console.log(error))
-})
+// // show page requesting
+// app.get('/restaurants/:id', (req, res) => {
+//   const id = req.params.id
+//   Restaurant.findById(id)
+//     .lean()
+//     .then(restaurants => {
+//       res.render('show', { restaurants })
+//     })
+//     .catch(error => console.log(error))
+// })
 
-// searching requesting
-app.get('/search', (req, res) => {
-  let keyword = req.query.keyword.trim()
-  Restaurant.find({ $or: [{ name: new RegExp(keyword, 'i') }, { category: new RegExp(keyword, 'i') }] })
-    .lean()
-    .then(restaurants => {
-      // exception
-      if (!restaurants.length) {
-        keyword = `你的收藏沒有"${keyword}"的相關項目唷！`
-      }
-      // do the searching
-      res.render('index', { restaurants, keyword })
-    })
-    .catch(error => console.log(error))
-})
+// // searching requesting
+// app.get('/search', (req, res) => {
+//   let keyword = req.query.keyword.trim()
+//   Restaurant.find({ $or: [{ name: new RegExp(keyword, 'i') }, { category: new RegExp(keyword, 'i') }] })
+//     .lean()
+//     .then(restaurants => {
+//       // exception
+//       if (!restaurants.length) {
+//         keyword = `你的收藏沒有"${keyword}"的相關項目唷！`
+//       }
+//       // do the searching
+//       res.render('index', { restaurants, keyword })
+//     })
+//     .catch(error => console.log(error))
+// })
 
-// to create page requesting
-app.get('/new', (req, res) => {
-  res.render('new')
-})
+// // to create page requesting
+// app.get('/new', (req, res) => {
+//   res.render('new')
+// })
 
-// create requesting
-app.post('/add', (req, res) => {
-  const name = req.body.name
-  const category = req.body.category
-  const location = req.body.location
-  const google_map = req.body.google_map
-  const phone = req.body.phone
-  const rating = req.body.rating
-  const image = req.body.image
-  const description = req.body.description
-  Restaurant.create({
-    name,
-    category,
-    location,
-    google_map,
-    phone,
-    rating,
-    image,
-    description
-  })
-    .then(() => res.redirect('/'))
-    .catch(error => console.log(error))
-})
+// // create requesting
+// app.post('/add', (req, res) => {
+//   const name = req.body.name
+//   const category = req.body.category
+//   const location = req.body.location
+//   const google_map = req.body.google_map
+//   const phone = req.body.phone
+//   const rating = req.body.rating
+//   const image = req.body.image
+//   const description = req.body.description
+//   Restaurant.create({
+//     name,
+//     category,
+//     location,
+//     google_map,
+//     phone,
+//     rating,
+//     image,
+//     description
+//   })
+//     .then(() => res.redirect('/'))
+//     .catch(error => console.log(error))
+// })
 
-// delete requesting
-app.delete('/restaurants/:id', (req, res) => {
-  const id = req.params.id
-  Restaurant.findById(id)
-    .then(restaurant => restaurant.remove())
-    .then(() => res.redirect('/'))
-    .catch(error => console.log(error))
-})
+// // delete requesting
+// app.delete('/restaurants/:id', (req, res) => {
+//   const id = req.params.id
+//   Restaurant.findById(id)
+//     .then(restaurant => restaurant.remove())
+//     .then(() => res.redirect('/'))
+//     .catch(error => console.log(error))
+// })
 
-// to edit page requesting
-app.get('/restaurants/:id/edit', (req, res) => {
-  const id = req.params.id
-  Restaurant.findById(id)
-    .lean()
-    .then(restaurant => res.render('edit', { restaurant }))
-    .catch(error => console.log(error))
-})
+// // to edit page requesting
+// app.get('/restaurants/:id/edit', (req, res) => {
+//   const id = req.params.id
+//   Restaurant.findById(id)
+//     .lean()
+//     .then(restaurant => res.render('edit', { restaurant }))
+//     .catch(error => console.log(error))
+// })
 
-// edit requesting
-app.put('/restaurants/:id', (req, res) => {
-  const id = req.params.id
-  Restaurant.findById(id)
-    .then(restaurant => {
-      restaurant = Object.assign(restaurant, req.body)
-      restaurant.save()
-    })
-    .then(() => res.redirect(`/restaurants/${id}`))
-    .catch(error => console.log(error))
-})
+// // edit requesting
+// app.put('/restaurants/:id', (req, res) => {
+//   const id = req.params.id
+//   Restaurant.findById(id)
+//     .then(restaurant => {
+//       restaurant = Object.assign(restaurant, req.body)
+//       restaurant.save()
+//     })
+//     .then(() => res.redirect(`/restaurants/${id}`))
+//     .catch(error => console.log(error))
+// })
 
 
 // --------------- localhost listenig --------------------
---
+++ routes/modules/CURD.js
@@ -0,0 +1,90 @@
+const express = require('express')
+const router = express.Router()
+const Restaurant = require('../../models/rList.js')
+
+// show page requesting
+router.get('/restaurants/:id', (req, res) => {
+  const id = req.params.id
+  Restaurant.findById(id)
+    .lean()
+    .then(restaurants => {
+      res.render('show', { restaurants })
+    })
+    .catch(error => console.log(error))
+})
+
+// searching requesting
+router.get('/search', (req, res) => {
+  let keyword = req.query.keyword.trim()
+  Restaurant.find({ $or: [{ name: new RegExp(keyword, 'i') }, { category: new RegExp(keyword, 'i') }] })
+    .lean()
+    .then(restaurants => {
+      // exception
+      if (!restaurants.length) {
+        keyword = `你的收藏沒有"${keyword}"的相關項目唷！`
+      }
+      // do the searching
+      res.render('index', { restaurants, keyword })
+    })
+    .catch(error => console.log(error))
+})
+
+// to create page requesting
+router.get('/new', (req, res) => {
+  res.render('new')
+})
+
+// create requesting
+router.post('/add', (req, res) => {
+  const name = req.body.name
+  const category = req.body.category
+  const location = req.body.location
+  const google_map = req.body.google_map
+  const phone = req.body.phone
+  const rating = req.body.rating
+  const image = req.body.image
+  const description = req.body.description
+  Restaurant.create({
+    name,
+    category,
+    location,
+    google_map,
+    phone,
+    rating,
+    image,
+    description
+  })
+    .then(() => res.redirect('/'))
+    .catch(error => console.log(error))
+})
+
+// delete requesting
+router.delete('/restaurants/:id', (req, res) => {
+  const id = req.params.id
+  Restaurant.findById(id)
+    .then(restaurant => restaurant.remove())
+    .then(() => res.redirect('/'))
+    .catch(error => console.log(error))
+})
+
+// to edit page requesting
+router.get('/restaurants/:id/edit', (req, res) => {
+  const id = req.params.id
+  Restaurant.findById(id)
+    .lean()
+    .then(restaurant => res.render('edit', { restaurant }))
+    .catch(error => console.log(error))
+})
+
+// edit requesting
+router.put('/restaurants/:id', (req, res) => {
+  const id = req.params.id
+  Restaurant.findById(id)
+    .then(restaurant => {
+      restaurant = Object.assign(restaurant, req.body)
+      restaurant.save()
+    })
+    .then(() => res.redirect(`/restaurants/${id}`))
+    .catch(error => console.log(error))
+})
+module.exports = router
